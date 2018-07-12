0b3412582477a749b6c7042c82ac609370664cc7
Krzysztof Łakwa <fenrir14@gmail.com>
2018-07-12 18:46:02 +0200

    Update AppReducer

--- client/modules/App/AppReducer.js
+++ client/modules/App/AppReducer.js
@@ -28,6 +28,7 @@ const AppReducer = (state = initialState, action) => {
 
 // Get showAddPost
 export const getShowAddPost = state => state.app.showAddPost;
+export const getShowEditPost = state => state.app.showEditPost;
 
 // Export Reducer
 export default AppReducer;
