9f7b4eb0d5fb50c08a59ada25575aae3235564a0
Krzysztof Łakwa <fenrir14@gmail.com>
2018-07-18 23:52:01 +0200

    Fix styles naming

--- client/modules/App/components/Navigation/Navigation.js
+++ client/modules/App/components/Navigation/Navigation.js
@@ -1,20 +1,16 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import { Link } from 'react-router';
 
 import styles from './Navigation.css';
 
 const Navigation = (props, context) => (
   <div className={styles.nav}>
-    <ul className={styles['nav--list']}>
-      <li><Link to="/home" className={styles['nav--list__element']}>Home</Link></li>
-      <li><Link to="/posts" className={styles['nav--list__element']}>Posts</Link></li>
-      <li><Link to="/about" className={styles['nav--list__element']}>About</Link></li>
+    <ul className={styles['nav__list']}>
+      <li><Link to="/home" className={styles['nav__element']}>Home</Link></li>
+      <li><Link to="/posts" className={styles['nav__element']}>Posts</Link></li>
+      <li><Link to="/about" className={styles['nav__element']}>About</Link></li>
     </ul>
   </div>
 );
-
-Navigation.PropTypes = {
-
-};
 
 export default Navigation;
